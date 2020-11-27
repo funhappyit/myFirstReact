354ba9aa076754317b5419a9dd449a8954789e0d
funhappyit <fun.happyit@gmail.com>
2020-11-27 17:39:41 +0900

    조건부 렌더링

--- src/Hello.js
+++ src/Hello.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
-function Hello({color,name}){
+function Hello({color, name, isSpecial}){
     //<div></div> jss
     //console.log(props);
-return <div style={{
-    color
- }}>안녕하세요 {name}</div>;
+return( 
+    <div style={{
+        color
+    }}>
+        {isSpecial && <b>*</b>}
+        안녕하세요 {name}
+        </div>
+    );
 }
 Hello.defaultProps = {
     name: '이름없음'
