da631c1a517c0ac34a5d3b1ececd4749325e69cc
funhappyit <fun.happyit@gmail.com>
2020-11-28 09:36:48 +0900

    다수의 inputs

--- src/inputSample.js
+++ src/inputSample.js
@@ -1,21 +1,32 @@
 import React, {useState} from 'react';
 
 function InputSample(){
-    const [text, setText] = useState('');
-
+    const[inputs, setInputs] = useState({
+        name: '',
+        nickname:'',
+    });
+    const{name, nickname} = inputs;
     const onChange = (e) => {
-       setText(e.target.value);
+     // console.log(e.target.name);
+     // console.log(e.target.value);
+        const { name, value} = e.target;
+        setInputs({
+            ...inputs,
+            //spread를 이용하여 객체를 복사한다음 덮어써준다.
+            [name]: value,
+        });
     }
     const onReset = () => {
-        setText('');
+       
     }
     return (
         <div>
-            <input onChange={onChange} value={text}/>
+            <input name="name" placeholder="이름" onChange={onChange} value={name}/>
+            <input name="nickname" placeholder="닉네임" onChange={onChange} value={nickname}/>
             <button onClick={onReset}>초기화</button>
             <div>
                 <b>값: </b>
-                {text}
+                {name} ({nickname})
             </div>
         </div>
     );
